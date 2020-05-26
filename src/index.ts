722c762caf2ec95878b97253a34bc3997698a61c
SoYoung210 <ethdud1@gmail.com>
2020-05-26 15:36:18 +0900

    feat: Add createSelectorCreator prototype

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-const createSelectorCreator = () => {
-  console.log('init');
+import { CombinedState, Reducer } from 'redux';
+
+// interface SelectorCreator<
+//   State = ReturnType<Reducer<CombinedState<any>>>,
+//   Name extends string = string
+// >
+// {
+//   name: Name;
+//   reducer: Reducer<State>;
+// }
+// https://github.com/reduxjs/redux-toolkit/issues/536
+const createSelectorCreator = (
+  name: string,
+) => (state: ReturnType<Reducer<CombinedState<any>>>) => {
+  return state[name];
 };
 
 export default createSelectorCreator;
